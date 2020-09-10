12a69a071d575e95ef2925dd8bbb57e6b8d9cfe5
Joe Meyer <joelawm@gmail.com>
2020-09-10 07:51:03 -0500

    Update to electron learning
    - Added Electron learning

---
+++ lessons/5. IPC Messaging/3. IPC Invoke & Handle/main.js
@@ -0,0 +1,60 @@
+// Modules
+const {app, BrowserWindow, dialog, ipcMain} = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+async function askFruit () {
+
+  let fruits = ['Apple', 'Orange', 'Grape']
+
+  let choice = await dialog.showMessageBox({
+    message: 'Pick a fruit:',
+    buttons: fruits
+  })
+
+  return fruits[choice.response]
+}
+
+ipcMain.handle('ask-fruit', e => {
+
+  return askFruit()
+
+})
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: false
+    }
+  })
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html')
+
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
