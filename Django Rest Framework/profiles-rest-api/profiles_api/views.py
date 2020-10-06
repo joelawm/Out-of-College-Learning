7ec78a2b1cde99f7b653e9f3fba418eb152116bb
Joe Meyer <joelawm@gmail.com>
2020-10-06 16:51:19 -0500

    Viewset example added

--- Django Rest Framework/profiles-rest-api/profiles_api/views.py
+++ Django Rest Framework/profiles-rest-api/profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -43,3 +44,30 @@ class HelloAPIView(APIView):
 	def delete(self, request, pk=None):
 		""" Deletes a object """
 		return Response({'method', 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+	""" Test API viewset """
+
+	serializer_class = serializers.HelloSerializer
+
+	def list(self, request):
+		""" Return a hello message """
+		a_viewset = [
+			'Uses HTTP methods as functions (get, post, patch, put, delete)',
+			'Is similar to a traditonal Django View',
+			'Gives you the most control over your application logic',
+			'Is mapped manually to URLs',
+		]
+
+		return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+	def create(self, request):
+		""" Create a new hello message """
+		serializer = self.serializer_class(data=request.data)
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = f'Hello{name}'
+			return Response({'message', message})
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
