5738b86347cf256775e911a55871f47f58f05620
Joe Meyer <joelawm@gmail.com>
2020-10-20 16:46:05 -0500

    Bi Directional proto file updated

--- gRPC Node JS Masterclass Build Modern API and Microservices/Bi Directional/server/proto/greet_grpc_pb.js
+++ gRPC Node JS Masterclass Build Modern API and Microservices/Bi Directional/server/proto/greet_grpc_pb.js
@@ -4,41 +4,41 @@
 var grpc = require('grpc');
 var proto_greet_pb = require('../proto/greet_pb.js');
 
-function serialize_greet_LongGreetRequest(arg) {
-  if (!(arg instanceof proto_greet_pb.LongGreetRequest)) {
-    throw new Error('Expected argument of type greet.LongGreetRequest');
+function serialize_greet_GreetEveryoneRequest(arg) {
+  if (!(arg instanceof proto_greet_pb.GreetEveryoneRequest)) {
+    throw new Error('Expected argument of type greet.GreetEveryoneRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_greet_LongGreetRequest(buffer_arg) {
-  return proto_greet_pb.LongGreetRequest.deserializeBinary(new Uint8Array(buffer_arg));
+function deserialize_greet_GreetEveryoneRequest(buffer_arg) {
+  return proto_greet_pb.GreetEveryoneRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_greet_LongGreetResponse(arg) {
-  if (!(arg instanceof proto_greet_pb.LongGreetResponse)) {
-    throw new Error('Expected argument of type greet.LongGreetResponse');
+function serialize_greet_GreetEveryoneResponse(arg) {
+  if (!(arg instanceof proto_greet_pb.GreetEveryoneResponse)) {
+    throw new Error('Expected argument of type greet.GreetEveryoneResponse');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_greet_LongGreetResponse(buffer_arg) {
-  return proto_greet_pb.LongGreetResponse.deserializeBinary(new Uint8Array(buffer_arg));
+function deserialize_greet_GreetEveryoneResponse(buffer_arg) {
+  return proto_greet_pb.GreetEveryoneResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 
 var GreetServiceService = exports.GreetServiceService = {
-  // Client Streaming API
-longGreet: {
-    path: '/greet.GreetService/LongGreet',
+  // Bi Di Streaming
+greetEveryone: {
+    path: '/greet.GreetService/GreetEveryone',
     requestStream: true,
-    responseStream: false,
-    requestType: proto_greet_pb.LongGreetRequest,
-    responseType: proto_greet_pb.LongGreetResponse,
-    requestSerialize: serialize_greet_LongGreetRequest,
-    requestDeserialize: deserialize_greet_LongGreetRequest,
-    responseSerialize: serialize_greet_LongGreetResponse,
-    responseDeserialize: deserialize_greet_LongGreetResponse,
+    responseStream: true,
+    requestType: proto_greet_pb.GreetEveryoneRequest,
+    responseType: proto_greet_pb.GreetEveryoneResponse,
+    requestSerialize: serialize_greet_GreetEveryoneRequest,
+    requestDeserialize: deserialize_greet_GreetEveryoneRequest,
+    responseSerialize: serialize_greet_GreetEveryoneResponse,
+    responseDeserialize: deserialize_greet_GreetEveryoneResponse,
   },
 };
 
