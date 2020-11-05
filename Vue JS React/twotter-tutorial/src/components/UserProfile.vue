ed3d9534315093a02c691294746af4ece49820d5
Joe Meyer <joelawm@gmail.com>
2020-11-05 00:04:27 -0600

    Added a components file

--- Vue JS React/twotter-tutorial/src/components/UserProfile.vue
+++ Vue JS React/twotter-tutorial/src/components/UserProfile.vue
@@ -8,16 +8,17 @@
       </div>
     </div>
     <div class="user-profile__twoots-wrapper">
-      <div class="user-profile__twoot" v-for="twoot in user.twoots" :key="twoot.id">
-        {{twoot.content}} 
-      </div>
+      <TwootItem v-for="twoot in user.twoots" :key="twoot.id" :username="user.username" :twoot="twoot"/>
     </div>
   </div>
 </template>
 
 <script>
+import TwootItem from "./TwootItem";
+
 export default {
   name: 'UserProfile',
+  components: { TwootItem },
   data() {
     return {
       followers: 0,
